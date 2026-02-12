4a2b5d029588dacb807cab33534359cedd1191ba
ignacio-gt <ignaciogtomg@gmail.com>
2026-02-12 17:07:31 -0500

    commit 1

---
+++ Backend/ProjectRentify/src/main/java/com/rentity/rentifyproject/entity/Auto.java
@@ -0,0 +1,37 @@
+package com.rentity.rentifyproject.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_vehiculo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Auto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_auto")
+	private long auto;
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String placa;
+	
+	@Column(name="id_vehiculo")
+	private Vehiculo vehiculo;
+	
+	private String color;
+	private long kilometraje;
+	private String estado;
+	
+}
---
+++ Backend/ProjectRentify/src/main/java/com/rentity/rentifyproject/entity/Vehiculo.java
@@ -0,0 +1,38 @@
+package com.rentity.rentifyproject.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_vehiculo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehiculo {
+	
+	@Id
+	@Column(name="id_vehiculo", unique = true, nullable = false)
+	private long vehiculo;
+	
+
+	private String marca;
+	private String model;
+	private String version;
+	private int year;
+	private String categoria;
+	private String carroceria;
+	private String combustible;
+	private String descripcion;
+	private BigDecimal precio;
+	private String estado;
+	private String imagen;
+}
